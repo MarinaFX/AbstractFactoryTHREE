00e8142f9a52627394ae241c9c2e40b9ce8b9244
Marina Moreira <38846018+MarinaFX@users.noreply.github.com>
2019-04-22 09:25:00 -0300

    Comentando Factories
    Comentando a ConcreteClienteFactory

--- src/Factories/ConcreteClienteFactory.java
+++ src/Factories/ConcreteClienteFactory.java
@@ -3,6 +3,11 @@ package Factories;
 import Clientes.Cliente;
 import Clientes.Genero;
 
+/**
+  * Seguindo o padrão A.F, essa é uma das fabricas (Classes instanciadoras) concretas, isto é, ela implementa a interface
+  * ClienteFactory e implementa o método abstrato da interface para instanciar um objeto do tipo Cliente. 
+  */
+
 public class ConcreteClienteFactory implements ClienteFactory {
 
     @Override
