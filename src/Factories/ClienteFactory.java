d0e95aca282cd5dd24d0ed710e512a026ca920f7
Marina Moreira <38846018+MarinaFX@users.noreply.github.com>
2019-04-22 09:22:31 -0300

    Comentando Factories
    Explicando cada Factory

--- src/Factories/ClienteFactory.java
+++ src/Factories/ClienteFactory.java
@@ -3,6 +3,10 @@ package Factories;
 import Clientes.Cliente;
 import Clientes.Genero;
 
+/**
+  * Essa interface funciona como a Abstract Factory. É a interface principal para as Concrete Factories implementarem ela. 
+  */
+
 public interface ClienteFactory {
     Cliente criaCliente(String name, String cpf, int age, Genero genero, String vip);
 }
